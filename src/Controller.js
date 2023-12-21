d49c931fb22e86efe78ba477d8087a0ce43d99f4
Brian Barasa <briannbig@gmail.com>
2023-12-21 09:17:53 +0300

    Feat: display fav color in uppercase. Learnt: adding angular js filters to expressions

--- src/Controller.js
+++ src/Controller.js
@@ -2,7 +2,7 @@ app.controller('angularFeastController', function ($scope) {
     $scope.name = "Brian";
     $scope.number = 3;
     $scope.favColor = "lightGreen";
-    $scope.quickColors = ['aqua', 'burlywood', 'coral', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
+    $scope.quickColors = ['aqua', 'bisque', 'burlywood', 'coral', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
         'darkorchid', 'fuchsia', 'lightBlue', 'lightGreen', 'royalblue', 'orange',]
     $scope.updateFavColor = function (color) {
         $scope.favColor = color
