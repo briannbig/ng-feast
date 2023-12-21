a2978a45ad223bac7923abbf131dd2aece211bcc
Brian Barasa <briannbig@gmail.com>
2023-12-21 10:43:44 +0300

    understanding scope. Learnt:

--- src/AppModule.js
+++ src/AppModule.js
@@ -1,1 +1,11 @@
 var app = angular.module('angularFeastApp', []);
+app.run(function ($rootScope) {
+    $rootScope.username = "Brian";
+    $rootScope.quickColors = ['aqua', 'lightBlue', 'burlywood', 'royalblue', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
+        'coral', 'darkorchid', 'fuchsia', 'bisque', 'lightGreen', 'orange',]
+    $rootScope.favColor = $rootScope.quickColors[Math.floor(Math.random() * $rootScope.quickColors.length)];
+    $rootScope.updateFavColor = function (color) {
+        $rootScope.favColor = color
+        $rootScope.colorFilter = ''
+    }
+})
--- src/Controller.js
+++ src/Controller.js
@@ -1,14 +1,8 @@
-app.controller('angularFeastController', function ($scope) {
-    $scope.name = "Brian";
-    $scope.number = 3;
-
-    $scope.quickColors = ['aqua', 'lightBlue', 'burlywood', 'royalblue', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
-        'coral', 'darkorchid', 'fuchsia', 'bisque', 'lightGreen', 'orange',]
-    $scope.favColor = $scope.quickColors[Math.floor(Math.random() * $scope.quickColors.length)];
-    $scope.updateFavColor = function (color) {
-        $scope.favColor = color
-        $scope.colorFilter = ''
-    }
+app.controller('squareController', function ($scope) {
+    $scope.number = Math.floor(Math.random() * 10)
+    
+})
+app.controller('colorCardController', function ($scope) {
     $scope.showQuickColors = true;
     $scope.toggleButtonGroup = function () {
         $scope.showQuickColors = $scope.showQuickColors ? false : true
