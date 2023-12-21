9bc1ae4f20b18837d22382e2d0991148af2d01f4
Brian Barasa <briannbig@gmail.com>
2023-12-21 08:41:18 +0300

    feat: update fav color on button click. Learnt: adding events, assigning functions to an event

--- src/Controller.js
+++ src/Controller.js
@@ -2,6 +2,9 @@ app.controller('angularFeastController', function ($scope) {
     $scope.name = "Brian";
     $scope.number = 3;
     $scope.favColor = "lightGreen";
-    $scope.quickColors = ['aqua',  'burlywood', 'coral', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
+    $scope.quickColors = ['aqua', 'burlywood', 'coral', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
         'darkorchid', 'fuchsia', 'lightBlue', 'lightGreen', 'royalblue', 'orange',]
+    $scope.updateFavColor = function (color) {
+        $scope.favColor = color
+    }
 });
