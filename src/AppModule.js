000798f6ad282378c83936aa7c76edc5b3e0c119
Brian Barasa <briannbig@gmail.com>
2023-12-21 18:34:11 +0300

    introduce user model

--- src/AppModule.js
+++ src/AppModule.js
@@ -1,7 +1,9 @@
 var app = angular.module('angularFeastApp', []);
+
 app.run(function ($rootScope) {
-    $rootScope.username = "Brian";
     $rootScope.quickColors = ['aqua', 'lightBlue', 'burlywood', 'royalblue', 'chocolate', 'crimson', 'darkgoldenrod', 'darkolivegreen',
         'coral', 'darkorchid', 'fuchsia', 'bisque', 'lightGreen', 'orange',]
     $rootScope.favColor = $rootScope.quickColors[Math.floor(Math.random() * $rootScope.quickColors.length)];
+
+    $rootScope.user = { "username": '', "Age": 18, 'favColor': $rootScope.favColor };
 })
