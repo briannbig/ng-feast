a7217809cf038ebfe8d9d6f3c89a961da6817dad
Brian Barasa <briannbig@gmail.com>
2023-12-21 07:58:57 +0300

    move app modeule, controller and directive in files

---
+++ src/Controller.js
@@ -0,0 +1,5 @@
+app.controller('angularFeastController', function ($scope) {
+    $scope.name = "Brian";
+    $scope.number = 3;
+    $scope.favColor = "lightGreen";
+});
---
+++ src/Directives.js
@@ -0,0 +1,5 @@
+app.directive("feastDirective", function () {
+    return {
+        template: "Feasting about to go down!"
+    };
+});
